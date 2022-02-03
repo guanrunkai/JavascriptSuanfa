a9682b2b8c1476e3a281ff733e0ca3a17f9d2091
guanrunkai <602768492@qq.com>
2022-02-03 12:44:07 +0800

    dev:html: 希尔排序

---
+++ Normal/4.ts
@@ -0,0 +1,36 @@
+// 希尔排序 在选择排序的基础上 利用gap 减少次数
+
+
+
+
+const shellSort =(arr)=>{
+
+  let len = arr.length
+
+  let gap = Math.floor(len/2)
+
+
+  while(gap>0){
+
+
+    for(let i = gap;i<len;i++){
+
+       let j = i
+       let tem = arr[i]
+      
+       while(j>gap-1 && arr[j-gap]>tem){
+         arr[j] = arr[j-gap]
+         
+         j-=gap
+       }
+      
+       arr[j] = tem
+    }
+    gap = Math.floor(gap/2)
+  }
+  
+  return arr
+
+}
+
+console.log(shellSort([8,4,7,2,9,1,3,5]));
